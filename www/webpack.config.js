9ac733d3f7eace0c26e4a511c9d851bc003be4ec
klangner <klangner@gmail.com>
2023-12-13 10:18:00 +0100

    upgraded webpack

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,14 +1,22 @@
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
   entry: "./bootstrap.js",
   output: {
-    path: path.resolve(__dirname, "../../docs"),
+    path: path.resolve(__dirname, "../docs"),
     filename: "bootstrap.js",
   },
   mode: "development",
   plugins: [
-    new CopyWebpackPlugin(['index.html', 'favicon.ico'])
+    new CopyPlugin({
+      patterns: [
+        { from: "assets", to: "assets" },
+        "index.html"
+      ],
+    })
   ],
+  experiments: {
+    syncWebAssembly: true,
+  },
 };
